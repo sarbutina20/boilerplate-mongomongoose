cd57bcba88d6950989ba253d18750543359c787f
sarbutina20 <sarbutina20@foi.hr>
2023-09-09 00:07:32 +0200

    Napravljena shema.

--- myApp.js
+++ myApp.js
@@ -8,6 +8,12 @@ mongoose.connect(process.env.MONGO_URI, {
 
 let Person;
 
+const personSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
