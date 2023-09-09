cf2da38bea575c1fddef0b536f670a7a45b4bd1b
sarbutina20 <sarbutina20@foi.hr>
2023-09-09 10:45:20 +0200

    Popravljeno ažuriranje osobe.

--- myApp.js
+++ myApp.js
@@ -72,8 +72,11 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
+  Person.findOneAndUpdate({ name: personName }, { age: ageToSet }, { new: true }, (err, data) => {
+    if (err) return console.error(err);
+    done(null, data);
+  });
 
-  done(null /*, data*/);
 };
 
 const removeById = (personId, done) => {
